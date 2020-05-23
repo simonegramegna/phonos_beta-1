33927b1ac5f0eb01f3910361c7335f8152678d17
Simox_lenovo <gramegna.simone@gmail.com>
2020-05-23 17:59:29 +0200

    rimosso modulo id non funzionanate..problemi su fopen

--- genera_id.h
+++
@@ -1,7 +0,0 @@
-#ifndef GENERA_ID_H
-#define GENERA_ID_H
-
-// funzione per generare l'identificativo
-int genera_id();
-
-#endif
--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "gestione_brani.h"
 #include "gestione_artisti.h"
 #include "gestione_album.h"
 
 int main(){
+
+	
+
 	// Funziona tutto
 	return 0;
 }
