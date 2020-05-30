dc13836ea30fdaad79dabdc4b990cdbef9d3c15b
Simox_lenovo <gramegna.simone@gmail.com>
2020-05-30 08:55:21 +0200

    modifiche minori su main.c

--- main.c
+++ main.c
@@ -2,41 +2,10 @@
 #include <stdlib.h>
 
 #include "interfaccia.h"
-#include "gestione_album.h"
-#include "recupero_dati.h"
-#include "gestione_brani.h"
-#include "gestione_playlist.h"
 
 int main()
 {
-	//interfaccia_principale();
-
-	brano b1;
-
-	scrivi_titolo_brano(&b1, "hello");
-	scrivi_durata_brano(&b1, 500);
-	scrivi_anno_brano(&b1, 1987);
-	scrivi_ascolti_brano(&b1, 5);
-
-	brano b2;
-
-	scrivi_titolo_brano(&b2, "hello");
-	scrivi_durata_brano(&b2, 500);
-	scrivi_anno_brano(&b2, 1987);
-	scrivi_ascolti_brano(&b2, 5);
-
-	aggiungi_brano(&b1);
-	aggiungi_brano(&b2);
-
-	mostra_brani();
-
-	backup_brani();
-
-	stampa_backup_brani();
-
-
-
-
+	interfaccia_principale();
 
 	return 0;
 }
