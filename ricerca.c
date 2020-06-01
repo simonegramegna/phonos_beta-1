11133de6279db235bc717ce5922dc11ec51a4710
Simox_lenovo <gramegna.simone@gmail.com>
2020-06-01 13:44:30 +0200

    modifiche minori su modulo ricerca

--- ricerca.c
+++ ricerca.c
@@ -1,21 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+// numero di secondi in un minuto
+#define MINUTO 60 
+
 #include "ricerca.h"
 #include "gestione_brani.h"
+#include "gestione_generi.h"
 #include "gestione_artisti.h"
+#include "gestione_playlist.h"
 
-/*--------------------
-	Ricerca brani
---------------------*/
-void ricerca_brani_per_titolo(char *titolo){
-	brano brano_corrente;
+
+/********************************************************
+ * 
+ *  Funzioni di ricerca per brani
+ * 
+********************************************************/
+
+void ricerca_brani_per_titolo( char *titolo_cercato )
+{
 	FILE *tabella_brani;
 
 	tabella_brani = fopen("brani.dat", "rb");
-	if(tabella_brani != NULL){
-		while(fread(&brano_corrente, sizeof(brano), 1, tabella_brani)){
-			if(strcmp(brano_corrente.titolo, titolo) == 0){
+
+	if(tabella_brani != NULL)
+	{
+		brano brano_corrente;
+
+		while( fread(&brano_corrente, sizeof(brano), 1, tabella_brani) )
+		{
+			// leggo il nome del brano appena letto
+			char nome_titolo_confronto[DIMTITOLO];
+			leggi_titolo_brano(brano_corrente, nome_titolo_confronto);
+
+			// verifico che i titoli coincidano
+			if( strcmp(nome_titolo_confronto, titolo_cercato) == 0 )
+			{
 				mostra_brano(brano_corrente);
 			}
 		}
@@ -24,14 +45,20 @@ void ricerca_brani_per_titolo(char *titolo){
 	fclose(tabella_brani);
 }
 
-void ricerca_brani_per_durata(int durata){
-	brano brano_corrente;
+void ricerca_brani_per_durata( int durata_cercata )
+{
 	FILE *tabella_brani;
 
 	tabella_brani = fopen("brani.dat", "rb");
-	if(tabella_brani != NULL){
-		while(fread(&brano_corrente, sizeof(brano), 1, tabella_brani)){
-			if(classe_durata_brano(brano_corrente.durata, durata)){
+
+	if(tabella_brani != NULL)
+	{
+		brano brano_corrente;
+
+		while(fread(&brano_corrente, sizeof(brano), 1, tabella_brani))
+		{
+			if( classe_durata_brano(brano_corrente.durata, durata_cercata) )
+			{
 				mostra_brano(brano_corrente);
 			}
 		}
@@ -40,32 +67,47 @@ void ricerca_brani_per_durata(int durata){
 	fclose(tabella_brani);
 }
 
-int classe_durata_brano(int durata, int classe_durata){
+int classe_durata_brano( int durata, int classe_durata)
+{
 	int risultato;
 
 	risultato = 0;
-	if(durata/60-classe_durata >= 0 && durata/60-classe_durata < 1){
+	if( durata/MINUTO - classe_durata >= 0 && durata/MINUTO - classe_durata < 1 )
+	{
 		risultato = 1;
 	}
-
+	
 	return risultato;
 }
 
-/*--------------------
-	Ricerca artisti
---------------------*/
-void ricerca_artisti_per_nome(char *nome){
-	artista artista_corrente;
+/********************************************************
+ * 
+ *  Funzioni di ricerca per artisti
+ * 
+********************************************************/
+
+void ricerca_artisti_per_nome( char *nome_cercato )
+{
 	FILE *tabella_artisti;
 
 	tabella_artisti = fopen("artisti.dat", "rb");
-	if(tabella_artisti != NULL){
-		while(fread(&artista_corrente, sizeof(artista), 1, tabella_artisti)){
-			if(strcmp(artista_corrente.nome, nome) == 0){
+
+	if(tabella_artisti != NULL)
+	{
+		artista artista_corrente;
+
+		while( fread(&artista_corrente, sizeof(artista), 1, tabella_artisti) )
+		{
+			// leggo il nome di un artista
+			char nome_artista_confronto[DIMNOME];
+			leggi_nome_artista(artista_corrente, nome_artista_confronto);
+
+			// verifico che i due nomi coincidano
+			if( strcmp(nome_artista_confronto, nome_cercato) == 0 )
+			{
 				mostra_artista(artista_corrente);
 			}
 		}
 	}
-
 	fclose(tabella_artisti);
 }
--- ricerca.h
+++ ricerca.h
@@ -1,8 +1,29 @@
 #ifndef RICERCA_H_
 #define RICERCA_H_
 
-void ricerca_brani_per_titolo(char *titolo);
-void ricerca_brani_per_durata(int durata);
-int classe_durata_brano(int durata, int classe_durata);
+
+/********************************************************
+ * 
+ *  Funzioni di ricerca per brani
+ * 
+********************************************************/
+
+// funzione per cercare un brano per titolo
+void ricerca_brani_per_titolo(char *titolo_cercato);
+
+// funzione per cercare un brano per durata
+void ricerca_brani_per_durata( int durata_cercata );
+
+// funzione per verificare l'aèèartenenza della durata di un brano ad una classe di equivalenza (di durata)
+int classe_durata_brano(int durata_cercata, int classe_durata);
+
+/********************************************************
+ * 
+ *  Funzioni di ricerca per artisti
+ * 
+********************************************************/
+
+// funzione per cercare un artista per nome
+void ricerca_artisti_per_nome( char *nome_cercato );
 
 #endif /* RICERCA_H_ */
