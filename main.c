d7fc29c0ff4a852f2d220988f7d7a5d70774e956
Simox_lenovo <gramegna.simone@gmail.com>
2020-06-02 12:47:41 +0200

    main per test su branoartista

--- main.c
+++ main.c
@@ -1,11 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "interfaccia.h"
+#include  "gestione_brani.h"
+#include "gestione_artisti.h"
+#include "gestione_relazioni.h"
 
 int main()
 {
-	interfaccia_principale();
+	brano b1, b2;
+
+	// scrivo informazioni del brano b1
+	scrivi_titolo_brano(&b1,"under pressure\0");
+	scrivi_durata_brano(&b1, 300);
+	scrivi_anno_brano(&b1, 1980);
+	scrivi_ascolti_brano(&b1, 2500);
+
+	// scrivo le informazioni del brano b2
+	scrivi_titolo_brano(&b2,"we are the champions\0");
+	scrivi_durata_brano(&b2, 190);
+	scrivi_anno_brano(&b2, 1977);
+	scrivi_ascolti_brano(&b2, 2500);
+
+	// inserisco i due brani nella base dati
+	aggiungi_brano(&b1);
+	aggiungi_brano(&b2);
+
+
+	// mostro i brani
+	mostra_brani();
+
+	// creo l'artista
+	artista a1;
+
+	scrivi_nome_artista(&a1,"farrokh\0");
+	scrivi_cognome_artista(&a1, "bulsara\0");
+	scrivi_nome_arte_artista(&a1, "Freddie Mercury\0");
+
+	// aggiungo l'artista 
+	aggiungi_artista(&a1);
+
+	printf("\n");
+
+	// mostro la gli artisti inseriti
+	mostra_artisti();
+
+	// creo le relazioni tra i due brani e l'artista creato
+	brano_artista r1, r2;
+
+
+
+
 
 	return 0;
 }
