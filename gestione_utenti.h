03a3331127cfa6e35e8da5f716b65f4de8eb8d59
Simox_lenovo <gramegna.simone@gmail.com>
2020-06-02 23:06:17 +0200

    modifiche sullo stile

--- gestione_utenti.h
+++ gestione_utenti.h
@@ -1,49 +1,99 @@
 #ifndef GESTIONE_UTENTI_H_
 #define GESTIONE_UTENTI_H_
 
+// definisco dimensione massima di un utente 
 #define DIMUSER 60
+
+// definisco dimesione massima di una password
 #define DIMPASS 60
 
-typedef struct {
+// definisco tipo di dato utente
+typedef struct{
+	
 	int id;
 	char username[DIMUSER];
 	char password[DIMPASS];
 	int admin;
 	int eliminato;
-} utente;
+
+}utente;
 
 /* -------------------------
 	Funzioni di lettura
 ------------------------- */
-int leggi_id_utente(utente utente_selezionato);
-void leggi_username_utente(utente utente_selezionato, char *username_letto);
-void leggi_password_utente(utente utente_selezionato, char *password_letta);
+
+// funzione per leggere l'id di un utente 
+int leggi_id_utente( utente utente_selezionato );
+
+// funzione per leggere l'username di un utente
+void leggi_username_utente( utente utente_selezionato, char *username_letto );
+
+// funzione per leggere la password di un utente
+void leggi_password_utente( utente utente_selezionato, char *password_letta );
+
+// funzione per leggere il flag 'admin' di un utente
 int leggi_admin_utente(utente utente_selezionato);
+
+// funzione per leggere il flag di eliminazione di un utente
 int leggi_flag_eliminato_utente(utente utente_selezionato);
 
 /* -------------------------
 	Funzioni di scrittura
 ------------------------- */
-void scrivi_id_utente(utente *utente_selezionato, int id);
-void scrivi_username_utente(utente *utente_selezionato, char *username);
-void scrivi_password_utente(utente *utente_selezionato, char *password);
-void scrivi_admin_utente(utente *utente_selezionato, int admin);
-void scrivi_flag_eliminato_utente(utente *utente_selezionato, int flag_eliminato);
+
+// funzione per scrivere l'id di un utente 
+void scrivi_id_utente( utente *utente_selezionato, int id );
+
+// funzione per scrivere l'username di un utente
+void scrivi_username_utente( utente *utente_selezionato, char *username );
+
+// funzione per scrivere la password di un utente 
+void scrivi_password_utente( utente *utente_selezionato, char *password );
+ 
+// funzione per scrivere il campo 'admin' di un utente
+void scrivi_admin_utente( utente *utente_selezionato, int admin );
+
+// funzione per scrivere il flag di eliminazione di un utente
+void scrivi_flag_eliminato_utente( utente *utente_selezionato, int flag_eliminato );
 
 /* -------------------------
 	Funzioni su file
 ------------------------- */
-int aggiungi_utente(utente *utente_selezionato);
+
+// funzione per aggiungere un utente nella tabella
+int aggiungi_utente( utente *utente_selezionato );
+
+// funzione per mostrare tutti gli utenti
 void mostra_utenti();
-void mostra_utente(utente utente_selezionato);
-long posizione_utente(int id_utente);
-int elimina_utente(int id_utente);
-utente cerca_utente(int id_utente);
-int modifica_utente(int id_utente, utente utente_modificato);
+
+// funzione per mostrare un determinato utente
+void mostra_utente( utente utente_selezionato );
+
+// funzione per cercare la posizione di un utente
+long posizione_utente( int id_utente );
+
+// funzione per eliminare un utente per id
+int elimina_utente( int id_utente );
+
+// funzione per leggere un utente per id
+utente cerca_utente( int id_utente );
+
+// fuznione per modificare un utente
+int modifica_utente( int id_utente, utente utente_modificato );
+
+// funzione per inserire un admin
 int inserisci_admin( utente* admin_inserito );
+
+// funzione per il controllo della presenza di un utente
 int controllo_presenza_utente(utente utente_selezionato);
-int controllo_credenziali(char *username, char *password);
-void autentica(utente utente_autenticato);
+
+// funzione per il controllo credenziali di un utente
+int controllo_credenziali( char *username, char *password );
+
+// funzione per autenticare un utente 
+void autentica( utente utente_autenticato );
+
+// funzione per leggere l'utente corrente
 utente leggi_utente_corrente();
 
 #endif /* GESTIONE_UTENTI_H_ */
