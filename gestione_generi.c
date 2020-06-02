c1c9c2b86015c04bf1bb0de9098bbafa431fc682
Simox_lenovo <gramegna.simone@gmail.com>
2020-06-02 23:41:18 +0200

    migliorie sullo stile ..

--- gestione_generi.c
+++ gestione_generi.c
@@ -1,151 +1,209 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "gestione_generi.h"
 #include "genera_id.h"
 
 /* -------------------------
 	Funzioni di lettura
 ------------------------- */
-int leggi_id_genere(genere genere_selezionato){
+
+int leggi_id_genere( genere genere_selezionato )
+{
 	return genere_selezionato.id;
 }
-void leggi_nome_genere(genere genere_selezionato, char *nome_letto){
+
+void leggi_nome_genere( genere genere_selezionato, char *nome_letto )
+{
 	strcpy(nome_letto, genere_selezionato.nome);
 }
-int leggi_flag_eliminato_genere(genere genere_selezionato){
+
+int leggi_flag_eliminato_genere( genere genere_selezionato )
+{
 	return genere_selezionato.eliminato;
 }
 
 /* -------------------------
 	Funzioni di scrittura
 ------------------------- */
-void scrivi_id_genere(genere *genere_selezionato, int id){
+
+void scrivi_id_genere( genere *genere_selezionato, int id )
+{
 	genere_selezionato->id = id;
 }
-void scrivi_nome_genere(genere *genere_selezionato, char *nome){
+
+void scrivi_nome_genere( genere *genere_selezionato, char *nome )
+{
     strcpy(genere_selezionato->nome, nome);
 }
-void scrivi_flag_eliminato_genere(genere *genere_selezionato, int flag_eliminato){
+
+void scrivi_flag_eliminato_genere( genere *genere_selezionato, int flag_eliminato )
+{
 	genere_selezionato->eliminato = flag_eliminato;
 }
 
 /* -------------------------
 	Funzioni su file
 ------------------------- */
-int aggiungi_genere(genere *genere_selezionato){
-	scrivi_id_genere(genere_selezionato, genera_id());
-	scrivi_flag_eliminato_genere(genere_selezionato, 0);
+
+int aggiungi_genere( genere *genere_selezionato )
+{
 	FILE *tabella_generi;
-	int aggiunto = 0;
+	int aggiunto;
 
 	tabella_generi = fopen("generi.dat", "ab");
-	if(tabella_generi != NULL){
+	aggiunto = 0;
+
+	if(tabella_generi != NULL)
+	{
+		scrivi_id_genere(genere_selezionato, genera_id());
+		scrivi_flag_eliminato_genere(genere_selezionato, 0);
+
 		fwrite(genere_selezionato, sizeof(genere), 1, tabella_generi);
 		aggiunto = 1;
 	}
-
 	fclose(tabella_generi);
 
 	return aggiunto;
 }
-void mostra_generi(){
-	genere genere_corrente;
-	FILE *tabella_generi;
 
+void mostra_generi()
+{
+	FILE *tabella_generi;
 	tabella_generi = fopen("generi.dat", "rb");
-	if(tabella_generi != NULL){
-		while(fread(&genere_corrente, sizeof(genere), 1, tabella_generi)){
+
+	if(tabella_generi != NULL)
+	{
+		genere genere_corrente;
+
+		while( fread(&genere_corrente, sizeof(genere), 1, tabella_generi) )
+		{
 			mostra_genere(genere_corrente);
 		}
 	}
 
 	fclose(tabella_generi);
 }
-void mostra_genere(genere genere_selezionato){
-	if(genere_selezionato.eliminato != 1){
+
+void mostra_genere( genere genere_selezionato )
+{
+	int flag_genere;
+	flag_genere = leggi_flag_eliminato_genere(genere_selezionato);
+
+	if( flag_genere != 1)
+	{
 		printf("ID: %d				\n", genere_selezionato.id);
 		printf("Nome: %s			\n", genere_selezionato.nome);
 		printf("Eliminato: %d		\n", genere_selezionato.eliminato);
 	}
 	printf("\n");
 }
-long posizione_genere(int id_genere){
+
+long posizione_genere( int id_genere) 
+{
     long posizione;
     FILE *tabella_generi;
-    genere genere_corrente;
-    int id_corrente;
+    
+  	tabella_generi = fopen("generi.dat", "rb");
+	posizione = -1;
 
-    posizione = -1;
-    tabella_generi = fopen("generi.dat", "rb");
-    if (tabella_generi != NULL){
-        while(!feof(tabella_generi) && posizione == -1){
+    if (tabella_generi != NULL)
+	{
+        while(!feof(tabella_generi) && posizione == -1)
+		{
+			int id_corrente;
+			genere genere_corrente;
+
             fread(&genere_corrente, sizeof(genere), 1, tabella_generi);
             id_corrente = leggi_id_genere(genere_corrente);
-            if(id_corrente == id_genere){
+
+            if(id_corrente == id_genere)
+			{
                 posizione = ftell(tabella_generi) - sizeof(genere); // perche' la ftell restituisce blocco successivo
             }
         }
     }
-
-    fclose(tabella_generi);
+	fclose(tabella_generi);
 
     return posizione;
 }
 int elimina_genere(int id_genere){
-	long posizione;
+	
 	FILE *tabella_generi;
 	int eliminato;
-	genere genere_trovato;
-
-	eliminato = 0;
-	posizione = posizione_genere(id_genere);
-	genere_trovato = cerca_genere(id_genere);
-	scrivi_flag_eliminato_genere(&genere_trovato, 1);
+	
 	tabella_generi = fopen("generi.dat", "rb+");
-	if(tabella_generi != NULL){
-		if(posizione != -1){
+	eliminato = 0;
+
+	if(tabella_generi != NULL)
+	{
+		long posizione;
+		posizione = posizione_genere(id_genere);
+
+		if(posizione != -1)
+		{
+			genere genere_trovato;
+			genere_trovato = cerca_genere(id_genere);
+
+			scrivi_flag_eliminato_genere(&genere_trovato, 1);
+
 			fseek(tabella_generi, posizione, SEEK_SET);
 			fwrite(&genere_trovato, sizeof(genere), 1, tabella_generi);
-		} else {
+		} 
+		else 
+		{
 			printf("genere non trovato \n");
 		}
 		eliminato = 1;
 	}
+	fclose(tabella_generi);
 
 	return eliminato;
 }
-genere cerca_genere(int id_genere){
-	long posizione;
+
+genere cerca_genere(int id_genere)
+{
 	FILE *tabella_generi;
 	genere genere_trovato;
 
-	posizione = posizione_genere(id_genere);
 	tabella_generi = fopen("generi.dat", "rb");
-	if(tabella_generi != NULL){
+
+	if(tabella_generi != NULL)
+	{
+		long posizione;
+		posizione = posizione_genere(id_genere);
+
 		fseek(tabella_generi, posizione, SEEK_SET);
 		fread(&genere_trovato, sizeof(genere), 1, tabella_generi);
 	}
+	fclose(tabella_generi);
 
 	return genere_trovato;
 }
-int modifica_genere(genere genere_modificato){
-	long posizione;
+
+int modifica_genere(genere genere_modificato)
+{
 	FILE *tabella_generi;
 	int modificato;
 	
-
-	modificato = 0;
-	int id_genere= leggi_id_genere(genere_modificato);
-	posizione = posizione_genere(id_genere);
-	
-
 	tabella_generi = fopen("generi.dat", "rb+");
-	if(tabella_generi != NULL){
+	modificato = 0;
+
+	if(tabella_generi != NULL)
+	{
+		long posizione;
+		int id_genere;
+		
+		id_genere = leggi_id_genere(genere_modificato);
+		posizione = posizione_genere(id_genere);
+
+
 		fseek(tabella_generi, posizione, SEEK_SET);
 		fwrite(&genere_modificato, sizeof(genere), 1, tabella_generi);
 		modificato = 1;
 	}
+	fclose(tabella_generi);
 
 	return modificato;
 }
--- gestione_generi.h
+++ gestione_generi.h
@@ -1,38 +1,68 @@
 #ifndef GESTIONE_GENERI_H_
 #define GESTIONE_GENERI_H_
 
+// dimensione massima del nome di un genere 
 #define DIMSTRING 30
 
-typedef struct {
+// definisco tipo di dato genere
+typedef struct{
+
 	int id;
 	char nome[DIMSTRING];
 	int eliminato;
-} genere;
+
+}genere;
 
 /* -------------------------
 	Funzioni di lettura
 ------------------------- */
-int leggi_id_genere(genere genere_selezionato);
-void leggi_nome_genere(genere genere_selezionato, char *nome_letto);
-int leggi_flag_eliminato_genere(genere genere_selezionato);
+
+// funzione per leggere l'id di un genere
+int leggi_id_genere( genere genere_selezionato );
+
+// funzione per leggere il nome di un genere
+void leggi_nome_genere( genere genere_selezionato, char *nome_letto );
+
+// funzione per leggere il flag di eliminazione di un genere
+int leggi_flag_eliminato_genere( genere genere_selezionato );
 
 /* -------------------------
 	Funzioni di scrittura
 ------------------------- */
-void scrivi_id_genere(genere *genere_selezionato, int id);
+
+// funzione per scrivere l'id di un genere
+void scrivi_id_genere( genere *genere_selezionato, int id );
+
+// funzione per scrivere il nome di un genere
 void scrivi_nome_genere(genere *genere_selezionato, char *nome);
+
+// funzione per scrivere il flag di eliminazione di genere
 void scrivi_flag_eliminato_genere(genere *genere_selezionato, int flag_eliminato);
 
 /* -------------------------
 	Funzioni su file
 ------------------------- */
-int aggiungi_genere(genere *genere_selezionato);
+
+// funzione per aggiungere un genere alla tabella
+int aggiungi_genere( genere *genere_selezionato );
+
+// funzione per mostrare tutti i generi nella tabella
 void mostra_generi();
-void mostra_genere(genere genere_selezionato);
-long posizione_genere(int id_genere);
-int elimina_genere(int id_genere);
-genere cerca_genere(int id_genere);
-int modifica_genere(genere genere_modificato);
+
+// funzione per mostrare un determinato genere
+void mostra_genere( genere genere_selezionato );
+
+// funzione per cercare la poszione di un genere nella tabella
+long posizione_genere( int id_genere );
+
+// funzione per eliminare un genere
+int elimina_genere( int id_genere );
+
+// funzione per leggere un genere con determinato id 
+genere cerca_genere( int id_genere );
+
+// funzione per modificare un genere
+int modifica_genere( genere genere_modificato );
 
 
 #endif /* GESTIONE_GENERI_H_ */
